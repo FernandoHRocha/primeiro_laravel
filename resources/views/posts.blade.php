e34d10600da3b4597d93db7aaa687a2f964d3f9c
FernandoHRocha <fhrlobacz@gmail.com>
2021-11-18 09:08:36 -0300

    Make a route and link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
+    <script src="js/app.js"></script>
+    <title>Larapio</title>
+</head>
+<body>
+    <h1>Fernando | <strong>Publicações</strong></h1>
+    <article>
+        <h2><a href="/post">Primeira Publicação</a></h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in deleniti reiciendis repudiandae, blanditiis culpa iure. Perferendis quo perspiciatis non similique officiis, libero voluptatibus maxime id tempora cum. Doloribus, perspiciatis!
+        </p>
+    </article>
+    <article>
+        <h2><a href="/post">Segunda Publicação</a></h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in deleniti reiciendis repudiandae, blanditiis culpa iure. Perferendis quo perspiciatis non similique officiis, libero voluptatibus maxime id tempora cum. Doloribus, perspiciatis!
+        </p>
+    </article>
+    <article>
+        <h2><a href="/post">Terceira Publicação</a></h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in deleniti reiciendis repudiandae, blanditiis culpa iure. Perferendis quo perspiciatis non similique officiis, libero voluptatibus maxime id tempora cum. Doloribus, perspiciatis!
+        </p>
+    </article>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
 
-Route::get('/oi', function () {
-    return ['Resposta' => 'Bom dia'];
+Route::get('/post', function () {
+    return view('post');
 });
