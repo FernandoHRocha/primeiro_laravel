8945b6626ca903edd39d2d0663c91a5495138314
FernandoHRocha <fhrlobacz@gmail.com>
2021-11-18 14:01:00 -0300

    Blade layouts Two Ways - First Way

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/app.css">
-    <script src="js/app.js"></script>
-</head>
+@extends('layout')
+
+@section('content')
+
 <body>
     <h1>Fernando  <strong>Publicações</strong></h1>
     <article>
@@ -19,4 +14,5 @@
     </article>
     <a href="/">Voltar</a>
 </body>
-</html>
+
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
+
+@section('content')
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/app.css">
-    <script src="js/app.js"></script>
     <title>Larapio</title>
 </head>
 <body>
@@ -24,4 +20,5 @@
         </article>
     @endforeach
 </body>
-</html>
+
+@endsection
