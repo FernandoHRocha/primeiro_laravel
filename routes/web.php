fddcc2adc00f8fa37a52690fda7895025aafb598
FernandoHRocha <fhrlobacz@gmail.com>
2021-11-18 10:15:36 -0300

    Using caching for expensive operations

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,10 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+    /*Get the path to the file from the {post} of the url
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";*/
 
-    if(! file_exists($path)) {
+    if(! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         /*Debug error
         dd('File does not exists.');*/
         /*Debug error
@@ -33,8 +34,25 @@ Route::get('posts/{post}', function ($slug) {
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    /*
+    Cache the page for reuse
+    Second parameter is in seconds, or using one of the above options:
+    now()->AddMinutes()
+    now()->AddHour()
+    now()->AddDay()
+    now()->AddWeeks()
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(0.1), function () use ($path)  {
+        
+        //Indicates when we are using this function, instead of a cached file
+        var_dump('file_get_contents');
+        
+        //Get the file from the given path
+        return file_get_contents($path);
+    });*/
+    //The arrow form for the function
+    $post = cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path));
 
+    //Return the file to be rendered, with the parameter declared in the post.blade.html file
     return view('post', [
         'post' => $post
     ]);
